8ba4dcc83b82b5ca141ba6432e15b463e871cf5d
Rishang <rishangbhavsarcs@gmail.com>
2021-05-31 18:20:47 +0530

    bash

---
+++ bash/bash-aliases.sh
@@ -0,0 +1,34 @@
+# clear
+alias clr='clear'
+
+# apt update
+alias update='sudo apt update'
+
+# apt upgrade
+alias upgrade='sudo apt update;sudo apt full-upgrade --fix-policy --fix-broken -y'
+
+# terminal theme
+alias ttheme='echo -e "go to http://mayccoll.github.io/Gogh/ \n press any key to continue"; read ;bash -c  "$(wget -qO- https://git.io/vQgMr)"'
+
+# IDA start
+# https://hex-rays.com/ida-free/#download
+alias ida='sudo /opt/idafree-*/./ida64'
+
+# terminal color
+alias term-color='x-www-browser "https://mayccoll.github.io/Gogh/" & bash -c "$(curl -sLo- https://git.io/vQgMr)"'
+
+# xdm
+# requires: https://xtremedownloadmanager.com/
+alias xdm="java -jar ~/xdman.jar"
+
+# doker rmi dangling
+alias dangling-rmi-docker='docker rmi `docker images --filter dangling=true -q`'
+
+# takeBreak alias
+# requires: tmux
+# requires: https://github.com/Rishang/scripts/tree/master/takeBreak
+alias getup='echo GetUp Every 30-min ;tmux new-session -d -s getUP ~/.takeBreak/getUp.sh'
+
+# the fuck
+# requires : pip3 install thefuck
+eval "$(thefuck --alias)"
---
+++ bash/bash-functions.sh
@@ -0,0 +1,50 @@
+# update all docker images
+function update-docker-pull() {
+	for image in  $(docker images --format '{{.Repository}} {{.Tag}} {{.Digest}}' | grep -v "none" | awk '{print $1":"$2}');do
+		 docker pull $image;
+	done
+}
+
+# update all pip packages
+function update-all-pip-packages() {
+    pip install -U pip
+    echo "This may create errors: Do you still want to continue"
+	read
+	pip list -o | sed -e '/\(Package.*\)\|\(----\)/d' | awk '{print $1}' > /tmp/pipr.txt && pip install -U -r /tmp/pipr.txt
+}
+
+# open multiple chrome tabs
+function open-multi-tabs() (
+
+    function _browser_open() {
+
+        if [[ `echo $1 | grep -E "^https?://"` ]];then
+            echo $1
+            x-www-browser $1 $2 &> /dev/null
+        elif ! [[ `echo $1 | grep -E ".+"` ]]; then
+        	sleep 0
+        else
+        	echo "$*"
+        	x-www-browser "https://www.google.com/search?q=$*" &> /dev/null
+        fi
+    }
+
+    local tab_path=/tmp/open-multi-tabs
+
+    if [ -t 0 ] ;then
+        vim $tab_path && touch $tab_path
+        if [[ `du $tab_path | grep -oE "^0"` ]];then exit 0;fi
+
+        x-www-browser &> /dev/null &
+        while read -r query;do
+            _browser_open $query
+        done < $tab_path
+
+    else
+    	x-www-browser &> /dev/null &
+        while read -r query;do
+            _browser_open $query
+        done
+    fi
+    echo
+)
