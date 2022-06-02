f376e64f1386e4a88a41440f73b5323e695c739c
Rishang <rishangbhavsarcs@gmail.com>
2022-06-02 20:37:56 +0530

    kubectl

--- rcfiles/completions.sh
+++ rcfiles/completions.sh
@@ -1,5 +1,7 @@
 # linux brew
 
+THIS_SHELL=$(ps -p $$ | awk '{print $4}' | tail -n 1)
+
 if type brew &>/dev/null
 then
   HOMEBREW_PREFIX="$(brew --prefix)"
@@ -19,3 +21,4 @@ fi
 [[ `type aws` ]] && complete -C `which aws_completer` aws
 [[ `type gf` ]] && complete -W "$(gf -list)" gf
 [[ `type terraform` ]] && complete -o nospace -C /usr/bin/terraform terraform
+[[ `type kubectl` ]] && source <(kubectl completion $THIS_SHELL)
